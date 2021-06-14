7f434b19ab89613e77dfa2485e583c7a3b867330
Michael Zion <michael@opslabs.io>
2021-06-14 12:19:01 +0300

    Google analytics

--- webui-vue/src/main.js
+++ webui-vue/src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuesax from "vuesax";
 import "vuesax/dist/vuesax.css";
+import VueAnalytics from 'vue-analytics';
 
 Vue.use(Vuesax);
+Vue.use(VueAnalytics, {
+  id: 'G-QQ0YMTLM6B',
+})
 Vue.config.productionTip = false
 
 new Vue({
